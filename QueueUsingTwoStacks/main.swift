13bc0b440171c315fa241abc2ff27acfbfe49d4a
slava bily <slavabily@me.com>
2021-12-06 15:33:51 +0200

    two stack queue is implemented based on stack implementation

--- QueueUsingTwoStacks/main.swift
+++ QueueUsingTwoStacks/main.swift
@@ -30,6 +30,48 @@ struct Stack: CustomStringConvertible {
     func peek() -> Int? {
         return items.first
     }
+}
+
+struct TwoStackQueue: CustomStringConvertible {
+    private var leftStack = Stack()
+    private var rightStack = Stack()
     
+    var isEmpty: Bool {
+        leftStack.items.isEmpty && rightStack.items.isEmpty
+    }
+    
+    var peek: Int? {
+        leftStack.items.isEmpty ? rightStack.items.first : leftStack.items.last
+    }
+    
+    var description: String {
+        if isEmpty {
+            return "Queue is empty"
+        }
+        var allElements:[Int] = []
+        if leftStack.items.isEmpty == false {
+            allElements.append(contentsOf: leftStack.items.reversed())
+        }
+        allElements.append(contentsOf: rightStack.items)
+        
+        return "----Queue start----\n"
+            + allElements.map ({ "\($0)" }).joined(separator: " -> ")
+            + "\n----Queue end----"
+    }
+    
+    mutating func enqueue(_ element: Int) {
+        leftStack.push(element)
+    }
+    
+    mutating func dequeue() -> Int? {
+        if isEmpty { return nil }
+        
+        if rightStack.items.isEmpty {
+            while !leftStack.items.isEmpty {
+                rightStack.push(leftStack.pop()!)
+            }
+        }
+        return rightStack.pop()
+    }
 }
 
